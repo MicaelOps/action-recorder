45b88aaecb7a49b1a3cfd2ac2408e001f659b605
MicaelOps <jthansmith55@gmail.com>
2026-01-27 13:43:51 +0000

    trying first inputs on a work laptop

--- main.cpp
+++ main.cpp
@@ -5,6 +5,27 @@
 #include <windows.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Sleep(3000);
+    INPUT inputs[4] = {};
+
+    // Ctrl down
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_CONTROL;
+
+    // V down
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = 'V';
+
+    // V up
+    inputs[2].type = INPUT_KEYBOARD;
+    inputs[2].ki.wVk = 'V';
+    inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    // Ctrl up
+    inputs[3].type = INPUT_KEYBOARD;
+    inputs[3].ki.wVk = VK_CONTROL;
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    SendInput(4, inputs, sizeof(INPUT));
     return 0;
 }
